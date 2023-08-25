4da69f67718da52386e93a21a4b8a9391d28e454
nixena <nikanozadze1010@gmail.com>
2023-08-25 15:07:10 +0400

    feat: write tanstakrouter configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,84 @@
-function App() {
+import {
+  RootRoute,
+  Outlet,
+  Route,
+  Router,
+  RouterProvider,
+  Navigate,
+} from "@tanstack/react-router";
+import { Home, Login, Preview, Profile, Register } from "./pages";
+
+const rootRoute = new RootRoute({
+  component: Root,
+});
+
+function Root() {
   return (
-    <div className="">
-    </div>
-  )
+    <>
+      <Outlet />
+    </>
+  );
 }
 
-export default App
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: () => <Navigate to="/login" />,
+});
+
+const loginRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "login",
+  component: Login,
+});
+
+const registerRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "register",
+  component: Register,
+});
+
+const homeRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "home",
+  component: Home,
+});
+
+const profileRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "profile",
+  component: Profile,
+});
+
+const previewRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "preview",
+  component: Preview,
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  loginRoute,
+  registerRoute,
+  homeRoute,
+  profileRoute,
+  previewRoute,
+]);
+
+const router = new Router({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
