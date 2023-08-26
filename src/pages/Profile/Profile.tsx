f50c3d6fb3b88aa3ff9f61e26a1e93a2f04f580d
nixena <nikanozadze1010@gmail.com>
2023-08-26 22:15:49 +0400

    feat: create user update form

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,25 @@
-import { Button, Header } from "../../components";
+import { Button, Header, Input } from "../../components";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { UploadImg } from "../../svg";
+import { ProfileType } from "../../types";
+import { profileSchema } from "../../schemas";
 
 const Profile = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<ProfileType>({
+    resolver: yupResolver(profileSchema),
+  });
+
+  const onSubmit: SubmitHandler<ProfileType> = async (data) => {
+    console.log(data);
+  };
   return (
-    <main className="w-full h-full bg-light-grey">
+    <main className="w-full min-h-screen bg-light-grey pb-20">
       <Header />
       <div className="w-full mt-4 px-4">
         <div className="w-full p-6 bg-white rounded-lg">
@@ -11,7 +27,7 @@ const Profile = () => {
           <h3 className="mt-1 text-grey text-base font-normal">
             Add your details to create a personal touch to your profile.
           </h3>
-          <form className="w-full mt-10">
+          <div className="w-full mt-10">
             <div className="mt-6 rounded-lg bg-light-grey p-5">
               <h3 className="text-grey text-base font-normal">
                 Profile picture
@@ -26,9 +42,50 @@ const Profile = () => {
                 Image must be below 1024x1024px. Use PNG or JPG format.
               </h3>
             </div>
+
+            <form
+              className="w-full mt-6 rounded-lg bg-light-grey p-5"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <label className="text-xs text-dark-grey font-normal">
+                First name*
+              </label>
+              <Input
+                register={register}
+                label="firstName"
+                placeholder="e.g. alex"
+                error={errors.firstName}
+                type="text"
+              />
+              <label className="text-xs text-dark-grey font-normal">
+                Last name*
+              </label>
+              <Input
+                register={register}
+                label="lastName"
+                placeholder="e.g. Dylon"
+                error={errors.lastName}
+                type="text"
+              />
+              <label className="text-xs text-dark-grey font-normal">
+                Email
+              </label>
+              <Input
+                register={register}
+                label="email"
+                placeholder="e.g. alex@email.com"
+                error={errors.email}
+                type="email"
+              />
+            </form>
             <div className="h-[1px] w-full bg-borders mt-6"></div>
-            <Button type="submit" text="Save" className=" bg-opacity-25 mt-4" />
-          </form>
+            <Button
+              type="submit"
+              text="Save"
+              className="mt-4"
+              onClick={handleSubmit(onSubmit)}
+            />
+          </div>
         </div>
       </div>
     </main>
